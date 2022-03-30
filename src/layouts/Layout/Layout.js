ae490023cf9c4b28b4d0d99f3de6017aefa19965
Yuliia Kharchenko <jkharchenko2008@gmail.com>
2022-03-30 18:48:07 +0300

    add font

--- src/layouts/Layout/Layout.js
+++ src/layouts/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Footer from "../Footer";
 import Header from "../Header";
+import styles from "./Layout.module.css";
 
 const Layout = ({ children }) => {
   return (
-    <div className="layout">
+    <div className={styles.layout}>
       <Header />
       <div>{children}</div>
       <Footer />
